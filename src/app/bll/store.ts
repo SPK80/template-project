dc6f6f068db0a8d7387ad1ff51b5c8a3c7419016
SPK80 <psukonkin@gmail.com>
2022-10-13 16:41:07 +0300

    refactor: create useAppSelector hook in store.ts format code

--- src/app/bll/store.ts
+++ src/app/bll/store.ts
@@ -1,19 +1,38 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux'
-import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
-import {appReducer} from "./appReducer";
-import {AppActionsType} from "./appActions";
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { appReducer } from "./appReducer";
+import { AppActionsType } from "./appActions";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
-    app: appReducer,
-    //add other reducers
-})
+  app: appReducer,
+  //add other reducers
+});
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
-export type AllActionsType =
-    |AppActionsType
-    //add other ActionsTypes
+export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AllActionsType = AppActionsType;
+//add other ActionsTypes
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AllActionsType>
-export type AppThunk<ReturnType = void> = ThunkAction<    ReturnType,    AppRootStateType,    unknown,    AllActionsType    >
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> =
+  useSelector;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export type AppDispatch = ThunkDispatch<
+  AppRootStateType,
+  unknown,
+  AllActionsType
+>;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppRootStateType,
+  unknown,
+  AllActionsType
+>;
